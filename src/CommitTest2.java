199c420e8d85319d42efd649e1c14e27ffdec736
lew <bit4math@gmail.com>
2024-05-06 17:59:28 +0900

    third commit

--- src/CommitTest2.java
+++ src/CommitTest2.java
@@ -2,7 +2,6 @@
 public class CommitTest2 {
 	public static void main(String[] args) {
 		System.out.println("CommitTest2");
-		System.out.println("CommitTest3");
 		System.out.println("CommitTest4");
 	}
 
